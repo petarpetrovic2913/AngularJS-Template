6419380293526c6b9915a7964fe544942ca62dda
petarpetrovic2913 <petar.petrovic.2913@metropolitan.ac.rs>
2017-03-11 04:26:35 +0100

    Second Commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+(function() {
+
+  "use strict";
+
+  angular
+    .module('ngClassifieds', ['ngMaterial'])
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('orange');
+    });
+        
+})();
+
+
